4dbe789324ee096e8e26f17a1f55b645bde1677d
mateuscamargo <mateuscamargolima@gmail.com>
2025-06-23 14:39:49 -0300

    Add arquitetura MVC e as classes

--- blogpessoal/main.ts
+++ blogpessoal/main.ts
@@ -3,16 +3,15 @@ import { AppModule } from './src/app.module';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 
-async function boostrap() {    
-    const app = await NestFactory.create(AppModule);
+async function boostrap() {
+  const app = await NestFactory.create(AppModule);
 
-    process.env.TZ = '-03:00'
+  process.env.TZ = '-03:00';
 
-    app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
 
-    app.enableCors();
+  app.enableCors();
 
-    await app.listen(process.env.PORT ?? 4000);
-
+  await app.listen(process.env.PORT ?? 4000);
 }
 boostrap();
